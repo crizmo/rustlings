44453a27d34c50802e5dd9fc7c6be8f56ec57060
Kurizu <83665497+crizmo@users.noreply.github.com>
2024-01-05 20:09:41 +0530

    🔥 Completed primitive_types

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,10 +5,10 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
-    let a = ???
+    let a = [0; 100]; // a is an array of 100 elements, all initialized to 0
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
--- exercises/primitive_types/primitive_types6.rs
+++ exercises/primitive_types/primitive_types6.rs
@@ -6,13 +6,13 @@
 // Execute `rustlings hint primitive_types6` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 #[test]
 fn indexing_tuple() {
     let numbers = (1, 2, 3);
     // Replace below ??? with the tuple indexing syntax.
-    let second = ???;
+    let second = numbers.1; // .1 is the second element of the tuple
 
     assert_eq!(2, second,
         "This is not the 2nd number in the tuple!")
